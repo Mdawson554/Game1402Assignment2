e54226439b8ed62f45c6adb8d6305b7003a10ab9
mdawson <m.dawson054@mybvc.ca>
2026-03-06 13:53:44 -0700

    crosshair and shooter camera integration

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,23 +5,31 @@ using UnityEngine.InputSystem;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] private InputAction shootInput;
+    [SerializeField] private InputAction aimInput;
+    
     [SerializeField] private Transform shootPoint;
     [SerializeField] private GameObject shootObject;
     [SerializeField] private float shootForce;
     [SerializeField] private float reloadSpeed;
-    
+    [SerializeField] private GameObject shooterCamera;
+    [SerializeField] private GameObject orbitalCamera;
     private GameObject _arrow;
+    
     private bool canShoot = true;
     
     void OnEnable()
     {
         shootInput.Enable();
-        shootInput.performed += Shoot;
+        shootInput.canceled += Shoot;
+        
+        aimInput.Enable();
+        aimInput.performed += AimWeapon;
     }
 
     void OnDisable()
     {
-        shootInput.performed -= Shoot;
+        shootInput.canceled -= Shoot;
+        aimInput.performed -= AimWeapon;
     }
 
     private void Shoot(InputAction.CallbackContext context)
@@ -35,6 +43,12 @@ public class Shooter : MonoBehaviour
             _arrow.GetComponent<Rigidbody>().AddForce(shootForce * shootPoint.forward);
         }
         canShoot = false;
+    }
+
+    private void AimWeapon(InputAction.CallbackContext context)
+    {
+       shooterCamera.SetActive(true);
+       orbitalCamera.SetActive(false);
     }
 
     private IEnumerator DestroyArrow()
